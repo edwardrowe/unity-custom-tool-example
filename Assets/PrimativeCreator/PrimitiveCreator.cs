b4fa649d5e9ba30b441dccfc443223a7fa473210
Edward Rowe <edward.l.rowe@gmail.com>
2016-08-14 14:54:39 -0400

    Rename tool to PrimitiveCreator, and allow spawning any primitive

---
+++ Assets/PrimativeCreator/PrimitiveCreator.cs
@@ -0,0 +1,54 @@
+﻿namespace RedBlueGames.ToolsExamples
+{
+    using UnityEditor;
+    using UnityEngine;
+    using System.Collections.Generic;
+
+    public class PrimitiveCreator
+    {
+        private static readonly Dictionary<Settings.PrimitiveColor, string> materialPaths =
+            new Dictionary<Settings.PrimitiveColor, string>()
+            {
+                { Settings.PrimitiveColor.Red, "Red" },
+                { Settings.PrimitiveColor.Green, "Green" },
+                { Settings.PrimitiveColor.Blue, "Blue" }
+            };
+
+        public static GameObject CreatePrimitive(Settings primitiveSettings)
+        {
+            var cube = GameObject.CreatePrimitive(primitiveSettings.PrimitiveType);
+            cube.transform.localScale = new Vector3(
+                primitiveSettings.UniformScale,
+                primitiveSettings.UniformScale,
+                primitiveSettings.UniformScale);
+
+            var materialResourcePath = materialPaths[primitiveSettings.Color];
+            var material = (Material)Resources.Load(materialResourcePath);
+            if (material == null)
+            {
+                Debug.LogError("No material resource found at resource path" +
+                    materialResourcePath);
+            
+                return null;
+            }
+
+            cube.GetComponent<Renderer>().material = material;
+
+            return cube;
+        }
+
+        public class Settings
+        {
+            public UnityEngine.PrimitiveType PrimitiveType;
+            public PrimitiveColor Color;
+            public float UniformScale = 1.0f;
+
+            public enum PrimitiveColor
+            {
+                Red,
+                Blue,
+                Green
+            }
+        }
+    }
+}
---
+++ Assets/PrimativeCreator/PrimitiveCreatorWindow.cs
@@ -0,0 +1,44 @@
+﻿namespace RedBlueGames.ToolsExamples
+{
+    using UnityEditor;
+    using UnityEngine;
+    using System.Collections;
+
+    public class PrimitiveCreatorWindow : EditorWindow
+    {
+        private PrimitiveCreator.Settings currentSettings;
+
+        [MenuItem("Window/Primitive Creator")]
+        public static void ShowCubeCreatorWindow()
+        {
+            EditorWindow.GetWindow<PrimitiveCreatorWindow>("Primitive Creator");
+        }
+
+        public void OnEnable()
+        {
+            this.currentSettings = new PrimitiveCreator.Settings();
+        }
+
+        public void OnGUI()
+        {
+            this.currentSettings.PrimitiveType = 
+                (UnityEngine.PrimitiveType)EditorGUILayout.EnumPopup(
+                new GUIContent("Primitive"),
+                this.currentSettings.PrimitiveType);
+
+            this.currentSettings.UniformScale = EditorGUILayout.FloatField(
+                new GUIContent("Uniform Scale"),
+                this.currentSettings.UniformScale);
+        
+            this.currentSettings.Color = (PrimitiveCreator.Settings.PrimitiveColor)
+                EditorGUILayout.EnumPopup(new GUIContent("Color"), this.currentSettings.Color);
+
+            if (GUILayout.Button("Create Primitive"))
+            {
+                GameObject cube = PrimitiveCreator.CreatePrimitive(this.currentSettings);
+
+                Selection.activeGameObject = cube;
+            }
+        }
+    }
+}
